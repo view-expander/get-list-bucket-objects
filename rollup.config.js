649e139fd845831b49ef72902fe7efa37f051c60
KIMURA Tetsuro <haribote.nobody@gmail.com>
2020-05-10 16:50:21 +0900

    chore: modify buildprocces.

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,27 @@
 import filesize from 'rollup-plugin-filesize'
 import progress from 'rollup-plugin-progress'
 import typescript from 'rollup-plugin-typescript2'
+import zip from 'rollup-plugin-zip'
 
 export default {
   input: './src/index.ts',
   output: [
     {
-      file: './dist/index.js',
-      format: 'cjs'
-    }
+      dir: 'dist',
+      format: 'cjs',
+    },
   ],
   plugins: [
     progress(),
     typescript({
       tsconfigOverride: {
-        exclude: ['__tests__']
-      }
+        exclude: ['__tests__'],
+      },
     }),
-    filesize()
-  ]
+    filesize(),
+    zip({
+      file: 'getListBucketObjects.zip',
+      dir: 'dist',
+    }),
+  ],
 }
